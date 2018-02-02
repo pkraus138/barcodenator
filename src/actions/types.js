48739036dff024e78ad99444b441487a259b72d4
Peter Kraus <fuudstamps@gmail.com>
2018-02-02 16:48:53 -0500

    adds Redux

---
+++ src/actions/types.js
@@ -0,0 +1,11 @@
+// Auth
+export const EMAIL_CHANGED = 'email_changed';
+export const PASSWORD_CHANGED = 'password_changed';
+export const LOGIN_USER_SUCCESS = 'login_user_success';
+export const LOGIN_USER_FAIL = 'login_user_fail';
+export const LOGIN_USER = 'login_user';
+
+// Data
+export const CHECK_BARCODE = 'check_barcode';
+export const CHECK_BARCODE_SUCCESS = 'check_barcode_success';
+export const CHECK_BARCODE_FAIL = 'check_barcode_fail';
